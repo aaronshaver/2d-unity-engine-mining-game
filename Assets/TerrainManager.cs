b8c511afeea554263cce2745d391285cab5103b1
Aaron Shaver <aaronshaverpdx@gmail.com>
2024-06-02 16:06:13 -0700

    add Perlin noise generation to terrain

--- Assets/TerrainManager.cs
+++ Assets/TerrainManager.cs
@@ -6,6 +6,8 @@ public class TerrainManager : MonoBehaviour
     public int gridWidth = 21;
     public int gridHeight = 21;
     public float tileSize = 1.0f;
+    public float noiseScale = 0.1f;
+    public float threshold = 0.5f;
 
     void Start()
     {
@@ -14,12 +16,21 @@ public class TerrainManager : MonoBehaviour
 
     void GenerateGrid()
     {
+        int randomSeed = System.DateTime.Now.Millisecond;
+        System.Random random = new System.Random(randomSeed);
+        float offsetX = random.Next(0, 10000);
+        float offsetY = random.Next(0, 10000);
+
         for (int x = -(gridWidth / 2); x < gridWidth / 2; x++)
         {
             for (int y = -gridHeight; y < 0; y++)
             {
-                Vector2 tilePosition = new Vector2(x * tileSize, y * tileSize);
-                Instantiate(tilePrefab, tilePosition, Quaternion.identity);
+                float perlinValue = Mathf.PerlinNoise((x * noiseScale) + offsetX, (y * noiseScale) + offsetY);
+                if (perlinValue > threshold)
+                {
+                    Vector2 tilePosition = new Vector2(x * tileSize, y * tileSize);
+                    Instantiate(tilePrefab, tilePosition, Quaternion.identity);
+                }
             }
         }
     }
